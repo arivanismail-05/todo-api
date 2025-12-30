bffe4d6134eb14eec0e1d3366f632a3a8e2f3650
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2025-12-30 18:26:56 +0300

    Implement Todo API with CRUD operations, add Sanctum for authentication, and configure routes.

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Todo::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+                'title' => 'required|string|max:255'
+        ]);
+        $todo = Todo::create($request->all());
+        return response()->json($todo, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json($todo);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+        $todo->update($request->all());
+        return response()->json($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json(null, 204);
+    }
+}
